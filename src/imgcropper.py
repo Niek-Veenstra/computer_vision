c6bb5911c751c75039f41b03fbbd068607e7850f
Niek Veenstra <niekveenstra14@gmail.com>
2025-10-22 16:56:07 +0100

    Commit image cropper.

--- src/imgcropper.py
+++ src/imgcropper.py
@@ -1,6 +1,11 @@
 import cv2;
 
-image = cv2.imread("image.py");
+
+image_str = input("Enter imaage from images dir.\n")
+
+image = cv2.imread(f"./images/{image_str}");
 r = cv2.selectROI("Select crop area",image);
 
+im_cropped = image[int(r[1]):int(r[1]+r[3]),int(r[0]):int(r[0]+r[2])];
 
+cv2.imshow("cropped img", im_cropped);
