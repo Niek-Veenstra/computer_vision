92f7d2c9147f0923219fefd7d54046fb96a03a31
Niek Veenstra <niekveenstra14@gmail.com>
2025-10-22 18:49:25 +0200

    Update and improve small tool.

--- src/imgcropper.py
+++ src/imgcropper.py
@@ -1,11 +1,16 @@
-import cv2;
+import cv2
+from cv2.typing import MatLike
 
 
 image_str = input("Enter imaage from images dir.\n")
+dataset_basename = input("Enter image basename.\n")
 
 image = cv2.imread(f"./images/{image_str}");
-r = cv2.selectROI("Select crop area",image);
+if image is None:
+    exit(1)
 
-im_cropped = image[int(r[1]):int(r[1]+r[3]),int(r[0]):int(r[0]+r[2])];
-
-cv2.imshow("cropped img", im_cropped);
+rois = cv2.selectROIs("Select crop areas",image,showCrosshair=True, fromCenter=False);
+for idx,(x,y,w,h) in enumerate(rois):
+    roi = image[y:y+h, x:x+w]
+    cv2.imwrite(f"./clipped_images/{dataset_basename}_{idx}.png",roi)
+cv2.waitKey(0);
