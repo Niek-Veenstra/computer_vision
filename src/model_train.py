3ca1592ac217fafc81da88dc36c46e0b2ad0df2e
Niek Veenstra <niekveenstra14@gmail.com>
2025-11-24 10:38:31 +0100

    woop.

--- src/model_train.py
+++
@@ -1,9 +0,0 @@
-import ultralytics.models as models
-model = models.YOLO()
-results = model.train(
-    data="dataset/dataset.yaml",
-    epochs=100,
-    imgsz=640,
-    batch=16,
-    name="symbol-detector"
-)
